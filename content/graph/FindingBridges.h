59a227fa690ed3b1231ae19c3b070b5b94372556
Divyansh Tripathi <55395696+theoden42@users.noreply.github.com>
2024-12-23 00:21:20 +0530

    add few graph algorithms

---
+++ content/graph/FindingBridges.h
@@ -0,0 +1,48 @@
+/**
+ * Author: cp-algorithms.com
+ * Date: 2024-12-21
+ * License:
+ * Source:
+ * Description: Finds bridges in a graph. A bridge is an edge which when removed
+ * Time: O(N + M)
+ * Usage:
+ * Status: works
+ */
+
+void IS_BRIDGE(int v, int to); // some function to process the found bridge
+int n;                         // number of nodes
+vector<vector<int>> adj;       // adjacency list of graph
+
+vector<bool> visited;
+vector<int> tin, low;
+int timer;
+
+void dfs(int v, int p = -1) {
+  visited[v] = true;
+  tin[v] = low[v] = timer++;
+  bool parent_skipped = false;
+  for (int to : adj[v]) {
+    if (to == p && !parent_skipped) {
+      parent_skipped = true;
+      continue;
+    }
+    if (visited[to]) {
+      low[v] = min(low[v], tin[to]);
+    } else {
+      dfs(to, v);
+      low[v] = min(low[v], low[to]);
+      if (low[to] > tin[v])
+        IS_BRIDGE(v, to);
+    }
+  }
+}
+void find_bridges() {
+  timer = 0;
+  visited.assign(n, false);
+  tin.assign(n, -1);
+  low.assign(n, -1);
+  for (int i = 0; i < n; ++i) {
+    if (!visited[i])
+      dfs(i);
+  }
+}
