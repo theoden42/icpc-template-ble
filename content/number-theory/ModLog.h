b8ac9517a0c8f4b84dd3e68f91e2348a2a684172
Divyansh Tripathi <55395696+theoden42@users.noreply.github.com>
2024-12-22 22:11:40 +0530

    modify number theory and graph sections

--- content/number-theory/ModLog.h
+++ content/number-theory/ModLog.h
@@ -32,14 +32,16 @@
  */
 #pragma once
 
+// Description: Returns the smallest $x > 0$ s.t. $a^x = b \pmod m$, or
+//  * $-1$ if no such $x$ exists.
 ll modLog(ll a, ll b, ll m) {
-	ll n = (ll) sqrt(m) + 1, e = 1, f = 1, j = 1;
-	unordered_map<ll, ll> A;
-	while (j <= n && (e = f = e * a % m) != b % m)
-		A[e * b % m] = j++;
-	if (e == b % m) return j;
-	if (__gcd(m, e) == __gcd(m, b)) 
-		rep(i,2,n+2) if (A.count(e = e * f % m))
-			return n * i - A[e];
-	return -1;
+  ll n = (ll)sqrt(m) + 1, e = 1, f = 1, j = 1;
+  unordered_map<ll, ll> A;
+  while (j <= n && (e = f = e * a % m) != b % m)
+    A[e * b % m] = j++;
+  if (e == b % m)
+    return j;
+  if (__gcd(m, e) == __gcd(m, b))
+    rep(i, 2, n + 2) if (A.count(e = e * f % m)) return n * i - A[e];
+  return -1;
 }
