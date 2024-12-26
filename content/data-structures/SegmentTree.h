fc6f669ae3de644887eb141fe42738c32a80d56e
Divyansh Tripathi <55395696+theoden42@users.noreply.github.com>
2024-12-26 15:55:08 +0530

    modify segment tree content

--- content/data-structures/SegmentTree.h
+++ content/data-structures/SegmentTree.h
@@ -3,31 +3,41 @@
  * Date: 2017-10-31
  * License: CC0
  * Source: folklore
- * Description: Zero-indexed max-tree. Bounds are inclusive to the left and
- * exclusive to the right. Can be changed by modifying T, f and unit. Time:
+ * Description:
  * O(\log N) Status: stress-tested
  */
 #pragma once
-
-struct Tree {
-  typedef int T;
-  static constexpr T unit = INT_MIN;
-  T f(T a, T b) { return max(a, b); } // (any associative fn)
-  vector<T> s;
-  int n;
-  Tree(int n = 0, T def = unit) : s(2 * n, def), n(n) {}
-  void update(int pos, T val) {
-    for (s[pos += n] = val; pos /= 2;)
-      s[pos] = f(s[pos * 2], s[pos * 2 + 1]);
+struct segtree {
+  int len, maxxn = 1e18;
+  vector<int> minn;
+  void init(int n) {
+    len = 1;
+    while (n > len)
+      len *= 2;
+    minn.assign(2 * len, maxxn);
   }
-  T query(int b, int e) { // query [b, e)
-    T ra = unit, rb = unit;
-    for (b += n, e += n; b < e; b /= 2, e /= 2) {
-      if (b % 2)
-        ra = f(ra, s[b++]);
-      if (e % 2)
-        rb = f(s[--e], rb);
+  void set(int i, int v, int x, int lx, int rx) {
+    if (rx - lx == 1)
+      minn[x] = v;
+    else {
+      int m = (lx + rx) / 2;
+      if (i < m)
+        set(i, v, 2 * x + 1, lx, m);
+      else
+        set(i, v, 2 * x + 2, m, rx);
+      minn[x] = min(minn[2 * x + 1], minn[2 * x + 2]);
     }
-    return f(ra, rb);
   }
+  void set(int i, int v) { set(i, v, 0, 0, len); }
+  int range(int l, int r, int x, int lx, int rx) {
+    if (l >= rx or r <= lx)
+      return maxxn;
+    if (l <= lx and rx <= r)
+      return minn[x];
+    int m = (lx + rx) / 2;
+    auto a = range(l, r, 2 * x + 1, lx, m);
+    auto b = range(l, r, 2 * x + 2, m, rx);
+    return min(a, b);
+  }
+  int range(int l, int r) { return range(l, r + 1, 0, 0, len); }
 };
